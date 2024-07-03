10974a8e9175ba6ece6425c1640d44790777ed82
deepaksoam313 <deepaksoam313@gmail.com>
2024-07-03 14:52:52 +0530

    first commit

---
+++ src/main/java/com/gl/GloSports/GloSportsApplication.java
@@ -0,0 +1,13 @@
+package com.gl.GloSports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloSportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloSportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gl/GloSports/GloSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.GloSports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloSportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
